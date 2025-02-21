a9086f5e653774e44383156e61b4753f7b9da311
Aimon Islam <dev@aimonext.com>
2025-02-21 16:28:27 +0600

    Fixed theme toggle functionality by updating main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
-
 import 'app/routes/app_pages.dart';
+import 'app/modules/more/controllers/theme_controller.dart';
 
 void main() {
+  final ThemeController themeController = Get.put(ThemeController());
+  
   runApp(
     GetMaterialApp(
       title: "Application",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+      themeMode: themeController.isDarkMode.value ? ThemeMode.dark : ThemeMode.light,
     ),
   );
 }
